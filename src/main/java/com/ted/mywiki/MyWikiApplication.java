e6ef20f1dccf7a64eac998d4772e33b7e6806aa5
Zhiyi Jin <victoryjzy@gmail.com>
2023-03-08 14:25:40 -0800

    Modify the start-up log

--- src/main/java/com/ted/mywiki/MyWikiApplication.java
+++ src/main/java/com/ted/mywiki/MyWikiApplication.java
@@ -1,13 +1,20 @@
 package com.ted.mywiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MyWikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MyWikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MyWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(MyWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Run successfully！！");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
