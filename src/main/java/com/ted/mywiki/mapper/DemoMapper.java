30c77500d474d6122b9d87a44f6f82fd1269078c
Zhiyi Jin <victoryjzy@gmail.com>
2023-03-08 21:08:20 -0800

    Integrate Mybatis generator, generate demo code

---
+++ src/main/java/com/ted/mywiki/mapper/DemoMapper.java
@@ -0,0 +1,22 @@
+package com.ted.mywiki.mapper;
+
+import com.ted.mywiki.domain.Demo;
+import com.ted.mywiki.domain.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+}
