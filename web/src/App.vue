d667e80236592411d922653e46a6d048047bbad6
Zhiyi Jin <victoryjzy@gmail.com>
2023-03-09 12:19:47 -0800

    Encapsulate header and footer as components

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,8 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo"/>
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Zhiyi Jin--WIKI
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
@@ -30,3 +16,17 @@
   }
 
 </style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+});
+</script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Zhiyi Jin -- WIKI
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+  props: {
+    msg: String,
+  },
+});
+</script>
