b3f95b0662b69133fceabe73ee928063b339611b
Zhiyi Jin <victoryjzy@gmail.com>
2023-03-09 18:55:16 -0800

    Copy the front-end and back-end codes of document management from category management

---
+++ src/main/java/com/ted/mywiki/controller/DocController.java
@@ -0,0 +1,52 @@
+package com.ted.mywiki.controller;
+
+import com.ted.mywiki.req.DocQueryReq;
+import com.ted.mywiki.req.DocSaveReq;
+import com.ted.mywiki.resp.CommonResp;
+import com.ted.mywiki.resp.DocQueryResp;
+import com.ted.mywiki.resp.PageResp;
+import com.ted.mywiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+}
